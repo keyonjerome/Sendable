86824de50169923c0dcb75e73325667ad2f4dcd3
Keyon-Jerome <keyonjerome@gmail.com>
2019-09-07 15:29:34 -0400

    Working—added sidenav component.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,9 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatSidenavModule} from '@angular/material/sidenav';
 // 1. Import the libs you need
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
@@ -14,7 +17,10 @@ import { AngularFireAuthModule } from '@angular/fire/auth';
 import { FirebaseConfig } from './app-config.model';
 import { APP_INITIALIZER } from '@angular/core';
 import { SideBarComponent } from './side-bar/side-bar.component';
+import {MatListModule} from '@angular/material/list';
 
+
+var firebase: FirebaseConfig;
 
 @NgModule({
   declarations: [
@@ -27,12 +33,16 @@ import { SideBarComponent } from './side-bar/side-bar.component';
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatListModule,
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireStorageModule,
-    AngularFireModule.initializeApp(firebaseConfig)
+    MatSidenavModule,
+    AngularFireModule.initializeApp(FirebaseConfig)
   ],
-  providers: [],
+  providers: [FirebaseConfig],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
