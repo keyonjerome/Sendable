ec8ef29c086c96606cc104d2f4e9f423d6a5dab1
Keyon-Jerome <keyonjerome@gmail.com>
2019-09-07 14:25:46 -0400

    Added routing.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,25 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import {HomeComponent} from './home/home.component';
+import { FrontPageComponent } from './front-page/front-page.component';
 
-
-const routes: Routes = [];
-
+// const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+    // data: { title: 'Heroes List' }
+  },
+  {
+    path:'',
+    component:FrontPageComponent
+  },
+  // { path: '',
+  //   redirectTo: '/front-page',
+  //   pathMatch: 'full'
+  // },
+  { path: '**',   component:FrontPageComponent }
+];
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { RouterModule, Routes } from '@angular/router';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
@@ -15,10 +15,12 @@ import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestor
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireAuthModule } from '@angular/fire/auth';
 import { FirebaseConfig } from './app-config.model';
+import {MatDialogModule} from '@angular/material/dialog';
 import { APP_INITIALIZER } from '@angular/core';
 import { SideBarComponent } from './side-bar/side-bar.component';
 import {MatListModule} from '@angular/material/list';
-
+import { HomeComponent } from './home/home.component';
+import {MatCardModule} from '@angular/material/card';
 
 var firebase: FirebaseConfig;
 
@@ -28,11 +30,14 @@ var firebase: FirebaseConfig;
     LoginComponent,
     FrontPageComponent,
     SideBarComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     MatButtonModule,
+    MatCardModule,
+    MatDialogModule,
     MatToolbarModule,
     MatIconModule,
     MatListModule,
@@ -40,7 +45,8 @@ var firebase: FirebaseConfig;
     AngularFireAuthModule,
     AngularFireStorageModule,
     MatSidenavModule,
-    AngularFireModule.initializeApp(FirebaseConfig)
+    AngularFireModule.initializeApp(FirebaseConfig),
+    RouterModule
   ],
   providers: [FirebaseConfig],
   bootstrap: [AppComponent]
