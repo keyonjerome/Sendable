1987c701b65579f390ccc58afd2d15d9566bbb57
Keyon-Jerome <keyonjerome@gmail.com>
2019-09-07 08:57:24 -0400

    Added auth service, Firebase imports.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,17 +6,38 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { FrontPageComponent } from './front-page/front-page.component';
 import {MatButtonModule} from '@angular/material/button';
+// 1. Import the libs you need
+import { AngularFireModule } from '@angular/fire';
+import { AngularFirestoreModule, AngularFirestore } from '@angular/fire/firestore';
+import { AngularFireStorageModule } from '@angular/fire/storage';
+import { AngularFireAuthModule } from '@angular/fire/auth';
+
+// 2. Add your credentials from step 1
+const firebaseConfig = {
+  apiKey: "AIzaSyDgeIPn7nET4ulXYPBtewfCE4ASPbaVeNw",
+  authDomain: "sendable.firebaseapp.com",
+  databaseURL: "https://sendable.firebaseio.com",
+  projectId: "sendable",
+  storageBucket: "sendable.appspot.com",
+  messagingSenderId: "704326462515",
+  appId: "1:704326462515:web:e1bb889472d5fa9a858746"
+};
+
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    FrontPageComponent
+    FrontPageComponent,
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    MatButtonModule
+    MatButtonModule,
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireStorageModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/auth.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Router} from '@angular/router';
+import {auth} from 'firebase/app';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+
+  constructor() { }
+}
